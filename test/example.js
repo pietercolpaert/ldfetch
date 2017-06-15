91239cd964a5ac0581d47f5d3bc5e9a8b730042e
Pieter Colpaert <pieter.colpaert@ugent.be>
2017-06-15 14:49:44 +0200

    Cleansed example a bit

--- test/example.js
+++ test/example.js
@@ -1,30 +1,26 @@
 var ldfetch = require('../lib/ldfetch.js');
-
 var fetch = new ldfetch();
+
+/**
+ * This example script shows how you can discover all subjects mentioned on the hydra:previous page of a Linked Data resource
+ */
+
+//Prefixes to be added to the N3 Store so we can query the data in an easier fashion
 fetch.addPrefix("hydra","http://www.w3.org/ns/hydra/core#");
 
-//var resourcePromise = fetch.get('https://linked.open.gent/parking');
-var resourcePromise = fetch.get('http://graph.spitsgids.be/connections/?departureTime=2017-06-12T11%3A00');
-var resourcePromise2 = fetch.get('http://graph.spitsgids.be/connections/?departureTime=2017-06-12T11%3A01');
-//var resourcePromise = fetch.get('http://fragments.dbpedia.org/2016-04/en');
-//var resourcePromise = fetch.get("http://schema.org/");
-//Prefixes to be added to the N3 Store so we can query the data in an easier fashion
-
-var cb = function (response) {
-  //Response contains a store and a url
-  //var prevpageTriple = response.store.getTriples(response.url,"hydra:previous"); //gets triples about this current page with a predicate hydra:previous
-  var prevpageTriple = response.store.getTriples(null,"http://www.w3.org/ns/hydra/core#next"); //gets triples about this current page with a predicate hydra:previous
-  console.dir(prevpageTriple);
-};
+//var url1 = 'http://linked.open.gent/parking';
+//but works as well flawlessly with this:
+var url1 = 'http://graph.spitsgids.be/'
 
 //Execute the request and do something with the response
-resourcePromise.then(response => {
-  cb(response);
-  resourcePromise2.then(cb, (error) => {
-    console.error(error);
+console.log("Requesting url1: " + url1);
+fetch.get(url1).then(response => {
+  console.log("Requesting the previous page: " + response.store.getTriples(null,"hydra:previous")[0].object);
+  fetch.get(response.store.getTriples(null,"hydra:previous")[0].object).then((response2) => {
+    //this does not work due to a bug in follow-redirects: https://github.com/olalonde/follow-redirects/issues/61
+    console.log("final url that was requested: " + response2.url);
+    //just return the subjects:
+    console.log(response2.store.getSubjects());
   });
-}, (error) => {
-  console.error(error);
 });
-
 
