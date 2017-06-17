d80eefaa58205167fb8e39a2ddc054d7ecfe36d3
Pieter Colpaert <pieter.colpaert@ugent.be>
2017-06-17 10:17:11 +0200

    Fixing small bug with response Url

--- test/example.js
+++ test/example.js
@@ -8,13 +8,14 @@ var fetch = new ldfetch();
 //Prefixes to be added to the N3 Store so we can query the data in an easier fashion
 fetch.addPrefix("hydra","http://www.w3.org/ns/hydra/core#");
 
-//var url1 = 'http://linked.open.gent/parking';
+var url1 = 'http://linked.open.gent/parking';
 //but works as well flawlessly with this:
-var url1 = 'http://graph.spitsgids.be/'
+//var url1 = 'http://graph.spitsgids.be/'
 
 //Execute the request and do something with the response
 console.log("Requesting url1: " + url1);
 fetch.get(url1).then(response => {
+  console.log("Redirected to: " + response.url);
   console.log("Requesting the previous page: " + response.store.getTriples(null,"hydra:previous")[0].object);
   fetch.get(response.store.getTriples(null,"hydra:previous")[0].object).then((response2) => {
     //this does not work due to a bug in follow-redirects: https://github.com/olalonde/follow-redirects/issues/61
