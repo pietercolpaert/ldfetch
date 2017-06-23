998cd049956ded654882e6ce9a2c36928ff32dd1
Pieter Colpaert <pieter.colpaert@ugent.be>
2017-06-23 17:25:18 +0200

    Added proof of concept of a URI checker

--- test/example.js
+++ test/example.js
@@ -18,7 +18,6 @@ fetch.get(url1).then(response => {
   console.log("Redirected to: " + response.url);
   console.log("Requesting the previous page: " + response.store.getTriples(null,"hydra:previous")[0].object);
   fetch.get(response.store.getTriples(null,"hydra:previous")[0].object).then((response2) => {
-    //this does not work due to a bug in follow-redirects: https://github.com/olalonde/follow-redirects/issues/61
     console.log("final url that was requested: " + response2.url);
     //just return the subjects:
     console.log(response2.store.getSubjects());
