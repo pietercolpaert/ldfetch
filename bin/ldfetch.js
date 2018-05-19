442cc765cdedf200beec00858cc18895a543b972
Pieter Colpaert <pieter.colpaert@ugent.be>
2018-05-19 14:31:53 +0200

    Catch errors

--- bin/ldfetch.js
+++ bin/ldfetch.js
@@ -38,19 +38,23 @@ var writer = n3.Writer(process.stdout, {end: false});
 var processPage = async function (pageUrl) {
   console.error('GET ' + pageUrl);
   var startTime = new Date();
-  var response = await fetch.get(pageUrl);
-  var endTime = new Date();
-  console.error('' + response.statusCode + ' ' +response.url + ' (' + (endTime.getTime() - startTime.getTime()) + 'ms)');
-  history.push(pageUrl);
-  history.push(response.url);
-  if (response.triples) {
-    writer.addQuads(response.triples);
-  }
-  for (var i in response.triples) {
-    var triple = response.triples[i];
-    if (program.predicates.includes(triple.predicate.value) && !history.includes(triple.object.value)) {
-      await processPage(triple.object.value);
+  try {
+    var response = await fetch.get(pageUrl);
+    var endTime = new Date();
+    console.error('' + response.statusCode + ' ' +response.url + ' (' + (endTime.getTime() - startTime.getTime()) + 'ms)');
+    history.push(pageUrl);
+    history.push(response.url);
+    if (response.triples) {
+      writer.addQuads(response.triples);
     }
+    for (var i in response.triples) {
+      var triple = response.triples[i];
+      if (program.predicates.includes(triple.predicate.value) && !history.includes(triple.object.value)) {
+        await processPage(triple.object.value);
+      }
+    }
+  } catch (e) {
+    console.error('Failed to retrieve ' + pageUrl + ': ' + e);
   }
 }
 
