382f714f60d88e0fcd78b295952f3bbaa33929e5
Pieter Colpaert <pieter.colpaert@ugent.be>
2026-09-04 17:03:05 +0200

    Migrating to rdf-writer-ts

--- bin/ldfetch.js
+++ bin/ldfetch.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var ldfetch = require('../lib/ldfetch.js');
 var fetch = new ldfetch();
-var n3 = require('n3');
+var rdfWriter = require('rdf-writer-ts');
 var program = require('commander').program;
 var path = require('path');
 var fs = require('fs');
@@ -27,7 +27,7 @@ let options = program.opts();
 
 if (!options.predicates)  options.predicates = [];
 
-//Prefixes to be added to the N3 Store so we can query the data in an easier fashion
+//Prefixes to be added to the writer so we can output the data in an easier fashion
 fetch.addPrefix("hydra","http://www.w3.org/ns/hydra/core#");
 
 if (!process.argv[2]) {
@@ -38,7 +38,7 @@ if (!process.argv[2]) {
 var history = [url];
 
 var url = process.argv[2];
-var writer = new n3.Writer(process.stdout, {end: false});
+var writer = new rdfWriter.Writer(process.stdout, {end: false});
 
 var processPage = async function (pageUrl) {
   console.error('GET ' + pageUrl);
